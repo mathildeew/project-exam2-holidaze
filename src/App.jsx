b09d69a2dab7a11a783209dbe139797a1df870f2
mathildeew <mathildewiik@hotmail.com>
2023-08-29 20:03:34 +0200

    adds page components to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import Profile from "./components/Profile";
+import Manage from "./components/Profile/Manage";
+import NotFound from "./components/NotFound";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="profile/manage" element={<Manage />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
