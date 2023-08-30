e94f8b1687a0484c7053bdc62d4882ad71adcea7
mathildeew <mathildewiik@hotmail.com>
2023-08-30 14:10:15 +0200

    adds content

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,3 +1,31 @@
+import { Link } from "react-router-dom";
+import { MainButton } from "../../styles/Buttons";
+import { RegisterContainer } from "./Register.style";
+
 export default function Register() {
-  return <></>;
+  return (
+    <>
+      <RegisterContainer>
+        <form>
+          <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" required></input>
+          </div>
+
+          <div>
+            <label for="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              minLength="8"
+              required
+            ></input>
+          </div>
+          <MainButton>Register</MainButton>
+        </form>
+        <p>Already have an account?</p>
+        <Link to={"/login"}>Log in here</Link>
+      </RegisterContainer>
+    </>
+  );
 }
