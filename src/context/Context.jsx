544b6506914b996cdeae44ef90909a108a1d6391
mathildeew <mathildewiik@hotmail.com>
2023-09-06 10:49:45 +0200

    adds reducer to context

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import { useReducer } from "react";
 import { useContext } from "react";
+import { reducer } from "./reducer";
 
 export function BookingProvider({ children }) {}
---
+++ src/context/reducer.js
@@ -0,0 +1,16 @@
+export function reducer(state, action) {
+  const addBooking = () => {};
+
+  const removeBooking = () => {};
+
+  switch (action.type) {
+    case "add":
+
+    case "remove":
+
+    case "clear":
+
+    default:
+      return state;
+  }
+}
