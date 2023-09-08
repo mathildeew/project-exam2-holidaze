417c6105a97229d42bec7c74dff7e0d239f777a8
mathildeew <mathildewiik@hotmail.com>
2023-09-08 19:01:02 +0200

    moves update avatar to own file

---
+++ src/components/Profile/UpdateAvatarAPI.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useLoggedIn } from "../../context/Context";
+import UseAPI from "../../hooks/useApi";
+
+export default function UpdateAvatarAPI({ data }) {
+  const [isLoggedIn, setIsLoggedIn] = useLoggedIn();
+  const { avatar, email, manager, name, token } = isLoggedIn;
+
+  const {
+    content: response,
+    isLoading,
+    isError,
+    isSuccess,
+  } = UseAPI(`https://api.noroff.dev/api/v1/holidaze/profiles/${name}/media`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  });
+
+  console.log(response);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setIsLoggedIn((isLoggedIn) => ({
+        ...isLoggedIn,
+        token: response.accessToken,
+        name: response.name,
+        email: response.email,
+        manager: response.venueManager,
+        avatar: response.avatar,
+      }));
+    }
+  }, [isSuccess]);
+}
