a53d8d99815239cdbd8ec4a5e74b389b7666aaad
mathildeew <mathildewiik@hotmail.com>
2023-09-20 21:21:13 +0200

    adds scroll to top on all routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/Context.jsx";
+import ScrollToTop from "./hooks/useScrollToTop.jsx";
 import { GlobalStyle } from "./styles/GlobalStyles.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <AuthProvider>
         <GlobalStyle />
+        <ScrollToTop />
         <App />
       </AuthProvider>
     </BrowserRouter>
