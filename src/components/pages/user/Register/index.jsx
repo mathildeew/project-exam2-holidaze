3ebc0991d708c32b449057af401fcd61a5fe8902
mathildeew <mathildewiik@hotmail.com>
2023-09-22 12:06:44 +0200

    clean up

--- src/components/pages/user/Register/index.jsx
+++ src/components/pages/user/Register/index.jsx
@@ -1,24 +1,34 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { MainButton } from "../../../../styles/Buttons";
-import { FormContainer } from "../User.style";
-import { useState } from "react";
-import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import apiEndpoints from "../../../../../endpoints.js/endpoints";
 import useApi from "../../../../hooks/useApi";
+import apiEndpoints from "../../../../../endpoints.js/endpoints";
+import { MainButton } from "../../../../styles/Buttons";
+import {
+  Banner,
+  Form,
+  FormContainer,
+  FormSection,
+  InputContainer,
+  Inputs,
+  Logo,
+  LogOrReg,
+} from "../User.style";
+import { ErrorMsg } from "../../../../styles/Text";
 
-export default function register() {
+export default function Register() {
   const navigate = useNavigate();
-  const [data, setData] = useState(null);
 
   const schema = yup.object({
-    name: yup.string().required("Please enter a username"),
+    name: yup
+      .string()
+      .matches(/^[a-zA-Z0-9_.-]*$/, "Name can only use a-z, and _")
+      .required("Please enter a username"),
     email: yup.string().required("Please enter your email"),
     password: yup
       .string()
-      .min(8, "Your password must be at least 8 characters")
+      .min(8, "Password must be at least 8 characters")
       .required("Please enter your password"),
     avatar: yup.string().url("Please enter a valid URL"),
     manager: yup.boolean(),
@@ -27,19 +37,18 @@ export default function register() {
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  const { isLoading, fetchApi, errorMsg, isError } = useApi();
+  const { isLoading, isSuccess, fetchApi, errorMsg, isError } = useApi();
 
   const onSubmit = async (formData) => {
-    setData(formData);
     const response = await fetchApi(apiEndpoints().register, "POST", formData);
+    console.log(response);
 
-    if (response.status === 200) {
+    if (response.status === 201) {
       setTimeout(() => {
-        navigate("/");
+        navigate("/user/login");
       }, 1000);
     }
   };
@@ -47,8 +56,8 @@ export default function register() {
   return (
     <>
       <FormContainer>
-        <section className="formSection">
-          <div className="top">
+        <FormSection>
+          <Logo>
             <svg
               id="Layer_1"
               data-name="Layer 1"
@@ -73,73 +82,96 @@ export default function register() {
               />
             </svg>
             <h1>Welcome to Holidaze!</h1>
-          </div>
+          </Logo>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="formContent">
-              <div className="flexCol">
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <InputContainer>
+              <Inputs>
                 <label htmlFor="name">Username</label>
                 <input
+                  name="name"
+                  type="text"
+                  placeholder="Your username"
                   {...register("name", { required: true, type: "text" })}
                 />
-                <p className="errorMsg">{errors.name?.message}</p>
-              </div>
+              </Inputs>
+              <ErrorMsg>{errors.name?.message}</ErrorMsg>
+            </InputContainer>
 
-              <div className="flexCol">
+            <InputContainer>
+              <Inputs>
                 <label htmlFor="email">Email</label>
                 <input
+                  name="email"
+                  type="email"
+                  placeholder="Your email"
                   {...register("email", { required: true, type: "email" })}
                 />
-                <p className="errorMsg">{errors.email?.message}</p>
-              </div>
+              </Inputs>
+              <ErrorMsg>{errors.email?.message}</ErrorMsg>
+            </InputContainer>
 
-              <div className="flexCol">
+            <InputContainer>
+              <Inputs>
                 <label htmlFor="password">Password</label>
                 <input
+                  name="password"
+                  type="password"
+                  placeholder="Your password"
                   {...register("password", {
                     required: true,
                     type: "password",
                   })}
                 />
-                <p className="errorMsg">{errors.password?.message}</p>
-              </div>
+              </Inputs>
+              <ErrorMsg>{errors.password?.message}</ErrorMsg>
+            </InputContainer>
 
-              <div className="flexCol">
+            <InputContainer>
+              <Inputs>
                 <label htmlFor="avatar">Avatar</label>
                 <input
+                  name="avatar"
+                  type="url"
+                  placeholder="Must be a URL"
                   {...register("avatar", {
                     type: "URL",
                   })}
                 />
-                <p className="errorMsg">{errors.avatar?.message}</p>
-              </div>
+              </Inputs>
+              <ErrorMsg>{errors.avatar?.message}</ErrorMsg>x
+            </InputContainer>
 
-              <div className="flexLine">
-                <input
-                  type="checkbox"
-                  onChange={() => !venueManager}
-                  {...register("manager", {
-                    type: "checkbox",
-                  })}
-                />
-                <label htmlFor="manager">Register as venue manager</label>
-              </div>
-              <p className="errorMsg">{errorMsg}</p>
-            </div>
+            <InputContainer>
+              <input
+                type="checkbox"
+                {...register("manager", {
+                  type: "checkbox",
+                })}
+              />
+              <label htmlFor="manager">Register as venue manager</label>
+            </InputContainer>
+
+            {isError && <p className="errorMsg">{errorMsg}</p>}
+
             <MainButton type="submit">
-              {isLoading ? "Registering..." : "Register"}
+              {isLoading
+                ? "Registering..."
+                : isSuccess
+                ? "Registered!"
+                : "Register"}
             </MainButton>
-          </form>
+          </Form>
 
-          <div className="loginContent flexLine">
+          <LogOrReg>
             <p>Already have an account?</p>
             <Link to={"/user/login"}>Log in here</Link>
-          </div>
-        </section>
+          </LogOrReg>
+        </FormSection>
 
-        <div className="banner">
+        <Banner>
           <span>Unwrap your holiday</span>
-        </div>
+        </Banner>
       </FormContainer>
     </>
   );
--- src/components/pages/user/login/index.jsx
+++ src/components/pages/user/login/index.jsx
@@ -1,34 +1,33 @@
-import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { MainButton } from "../../../../styles/Buttons";
-import { FormContainer } from "../User.style";
+import { useLoggedIn } from "../../../../context/Context";
 import useApi from "../../../../hooks/useApi";
 import apiEndpoints from "../../../../../endpoints.js/endpoints";
-import { set } from "../../../../js/storage/localStorage";
-import { useLoggedIn } from "../../../../context/Context";
+import { MainButton } from "../../../../styles/Buttons";
+import {
+  FormContainer,
+  LogOrReg,
+  Inputs,
+  Form,
+  Logo,
+  Banner,
+  InputContainer,
+  FormSection,
+} from "../User.style";
+import { ErrorMsg } from "../../../../styles/Text";
 
 export default function Login() {
+  const navigate = useNavigate();
   const {
-    isLoggedIn,
     setIsLoggedIn,
-    isManager,
     setIsManager,
-    token,
     setToken,
-    avatar,
     setAvatar,
-    name,
     setName,
-    email,
     setEmail,
   } = useLoggedIn();
-
-  const [data, setData] = useState("");
-  const [btnText, setBtnText] = useState("Log in");
-  const navigate = useNavigate();
 
   const schema = yup.object({
     email: yup
@@ -37,22 +36,21 @@ export default function Login() {
       .required("Please enter your email"),
     password: yup
       .string()
-      .min(8, "Must be at least 8 characters")
+      .min(8, "Password must be at least 8 characters")
       .required("Please enter your password"),
   });
 
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  const { isLoading, fetchApi, errorMsg, isError } = useApi();
+  const { isLoading, isSuccess, fetchApi, errorMsg, isError } = useApi();
 
   const onSubmit = async (formData) => {
-    setData(formData);
     const response = await fetchApi(apiEndpoints().login, "POST", formData);
+    console.log(response);
 
     if (response.status === 200) {
       setIsLoggedIn(true);
@@ -66,86 +64,82 @@ export default function Login() {
         navigate("/");
       }, 1000);
     }
-
-    if (!errorMsg) {
-    }
   };
 
   return (
-    <>
-      <FormContainer>
-        <section className="formSection">
-          <div className="top">
-            <svg
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="117.594"
-              height="57.716"
-              viewBox="0 0 117.594 57.716"
-            >
-              <path
-                id="Path_71"
-                data-name="Path 71"
-                d="M84.686,15A57.748,57.748,0,0,0,27.06,72.274c5.88-1.068,9.463-3.977,13.745-7.493,5.972-4.9,12.716-10.439,25.891-10.439s19.938,5.541,25.891,10.439c5.4,4.437,9.684,7.935,18.909,7.935S125,69.218,130.4,64.781a52.756,52.756,0,0,1,9.96-6.9A57.747,57.747,0,0,0,84.686,15Z"
-                transform="translate(-25.135 -15)"
-                fill="none"
+    <FormContainer>
+      <FormSection>
+        <Logo>
+          <svg
+            id="Layer_1"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="117.594"
+            height="57.716"
+            viewBox="0 0 117.594 57.716"
+          >
+            <path
+              id="Path_71"
+              data-name="Path 71"
+              d="M84.686,15A57.748,57.748,0,0,0,27.06,72.274c5.88-1.068,9.463-3.977,13.745-7.493,5.972-4.9,12.716-10.439,25.891-10.439s19.938,5.541,25.891,10.439c5.4,4.437,9.684,7.935,18.909,7.935S125,69.218,130.4,64.781a52.756,52.756,0,0,1,9.96-6.9A57.747,57.747,0,0,0,84.686,15Z"
+              transform="translate(-25.135 -15)"
+              fill="none"
+            />
+            <path
+              id="Path_72"
+              data-name="Path 72"
+              d="M107.66,34.233c-3.115-2.508-6.636-5.348-13.465-5.376A38.882,38.882,0,0,0,56.473,0C35.456,0,18.288,16.49,17.583,37.026A23.265,23.265,0,0,1,13.6,34.233C10.465,31.707,6.926,28.848.01,28.848v5.7c4.842,0,7.089,1.8,9.925,4.094,3.135,2.527,6.674,5.386,13.59,5.386s10.455-2.859,13.59-5.386c2.836-2.289,5.083-4.094,9.925-4.094s7.089,1.8,9.925,4.094c3.135,2.527,6.674,5.386,13.59,5.386s10.465-2.859,13.59-5.386c.868-.7,1.688-1.358,2.537-1.928a12.226,12.226,0,0,1,7.4-2.166c.405,0,.781.019,1.148.038,4.07.266,6.183,1.957,8.787,4.056,3.135,2.527,6.684,5.386,13.59,5.386v-5.7c-4.852,0-7.089-1.8-9.925-4.094Zm-27.19,0c-2.836,2.289-5.083,4.094-9.925,4.094s-7.089-1.8-9.925-4.094c-3.135-2.527-6.674-5.386-13.59-5.386s-10.465,2.859-13.59,5.386c-2.247,1.814-4.128,3.315-7.215,3.866A30.3,30.3,0,0,1,85.7,30.671,27.884,27.884,0,0,0,80.47,34.233Z"
+              transform="translate(-0.01 6.845)"
+              fill="#4b3d60"
+            />
+          </svg>
+          <h1>Welcome to Holidaze!</h1>
+        </Logo>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <Inputs>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="post@noroff.no"
+                {...register("email", { required: true, type: "email" })}
               />
-              <path
-                id="Path_72"
-                data-name="Path 72"
-                d="M107.66,34.233c-3.115-2.508-6.636-5.348-13.465-5.376A38.882,38.882,0,0,0,56.473,0C35.456,0,18.288,16.49,17.583,37.026A23.265,23.265,0,0,1,13.6,34.233C10.465,31.707,6.926,28.848.01,28.848v5.7c4.842,0,7.089,1.8,9.925,4.094,3.135,2.527,6.674,5.386,13.59,5.386s10.455-2.859,13.59-5.386c2.836-2.289,5.083-4.094,9.925-4.094s7.089,1.8,9.925,4.094c3.135,2.527,6.674,5.386,13.59,5.386s10.465-2.859,13.59-5.386c.868-.7,1.688-1.358,2.537-1.928a12.226,12.226,0,0,1,7.4-2.166c.405,0,.781.019,1.148.038,4.07.266,6.183,1.957,8.787,4.056,3.135,2.527,6.684,5.386,13.59,5.386v-5.7c-4.852,0-7.089-1.8-9.925-4.094Zm-27.19,0c-2.836,2.289-5.083,4.094-9.925,4.094s-7.089-1.8-9.925-4.094c-3.135-2.527-6.674-5.386-13.59-5.386s-10.465,2.859-13.59,5.386c-2.247,1.814-4.128,3.315-7.215,3.866A30.3,30.3,0,0,1,85.7,30.671,27.884,27.884,0,0,0,80.47,34.233Z"
-                transform="translate(-0.01 6.845)"
-                fill="#4b3d60"
+            </Inputs>
+            <ErrorMsg>{errors.email?.message}</ErrorMsg>
+          </InputContainer>
+
+          <InputContainer>
+            <Inputs>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Your password"
+                {...register("password", {
+                  required: true,
+                  type: "password",
+                })}
               />
-            </svg>
-            <h1>Welcome to Holidaze!</h1>
-          </div>
+            </Inputs>
+            <ErrorMsg>{errors.password?.message}</ErrorMsg>
+          </InputContainer>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="formContent">
-              <div className="flexCol">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="email"
-                  placeholder="post@noroff.no"
-                  {...register("email", { required: true, type: "email" })}
-                />
-                <p className="errorMsg">{errors.email?.message}</p>
-              </div>
+          {isError && <ErrorMsg>{errorMsg}</ErrorMsg>}
 
-              <div className="flexCol">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  placeholder="Your password"
-                  {...register("password", {
-                    required: true,
-                    type: "password",
-                  })}
-                />
-                {isError && <p className="errorMsg">{errorMsg}</p>}
-              </div>
-            </div>
-            <MainButton type="submit">
-              {isLoading ? "Logging in..." : "Log in"}
-            </MainButton>
-          </form>
+          <MainButton type="submit">
+            {isLoading ? "Logging in..." : isSuccess ? "Logged in!" : "Log in"}
+          </MainButton>
+        </Form>
 
-          <div className="loginContent flexLine">
-            <p>Dont have an account?</p>
-            <Link to={"/user/register"}>Register here</Link>
-          </div>
+        <LogOrReg>
+          <p>Dont have an account?</p>
+          <Link to={"/user/register"}>Register here</Link>
+        </LogOrReg>
+      </FormSection>
 
-          {/* <div className="footer">
-            <p>&copy;2023 Holidaze - Mathilde Elinor Wiik</p>
-          </div> */}
-        </section>
-
-        <div className="banner">
-          <span>Unwrap your holiday</span>
-        </div>
-      </FormContainer>
-    </>
+      <Banner>
+        <span>Unwrap your holiday</span>
+      </Banner>
+    </FormContainer>
   );
 }
