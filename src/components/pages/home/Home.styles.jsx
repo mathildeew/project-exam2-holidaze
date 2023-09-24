4d78898f8357020df29a89c5ffe8578d3e9f47ca
mathildeew <mathildewiik@hotmail.com>
2023-09-24 16:56:13 +0200

    sorted imports, deletes unused components, moved inputcontainer style to seperate file

--- src/components/pages/home/Home.styles.jsx
+++ src/components/pages/home/Home.styles.jsx
@@ -6,36 +6,12 @@ export const HomeContainer = styled.main`
   height: 100%;
 `;
 
-export const FilterButton = styled.div`
-  background-color: var(--orange);
-  padding: 15px;
-  border-radius: 100px;
-  position: fixed;
-  right: 10px;
-  bottom: 10px;
-  z-index: 2;
-
-  svg {
-    color: var(--primary);
-    font-size: 2rem;
-  }
-`;
-
-export const FilterContent = styled.div`
-  padding: 20px;
-
-  section {
-    margin-bottom: 20px;
-  }
-`;
-
 export const Hero = styled.section`
   width: 100%;
   height: 455px;
-  background-image: url(/public/daniel-olah-f0P7y3swnZU-unsplash.jpg);
-  background-position: center bottom;
+  background: url(/public/daniel-olah-f0P7y3swnZU-unsplash.jpg) no-repeat center
+    bottom;
   background-size: cover;
-  background-repeat: no-repeat;
   margin-bottom: 50px;
 
   ${displayFlex({
@@ -67,42 +43,5 @@ export const Search = styled.div`
       padding: 30px;
       width: 50%;
     }
-  }
-`;
-
-export const InputContainer = styled.div`
-  color: white;
-  background: rgba(75, 61, 96, 0.6);
-  border-radius: 10px;
-  padding: 10px;
-
-  input {
-    background: none;
-    border: none;
-    padding: 3px;
-
-    ::placeholder {
-      color: var(--light-yellow);
-      font-size: 1.6rem;
-    }
-
-    :focus {
-      color: white;
-      font-size: 1.6rem;
-
-      outline: none;
-    }
-  }
-  input[type="search"]::-webkit-search-cancel-button {
-    display: none;
-  }
-  input[value] {
-    color: white;
-  }
-
-  svg {
-    color: var(--light-yellow);
-    font-size: 2rem;
-    margin-right: 10px;
   }
 `;
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -3,25 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import useApi from "../../../hooks/useApi";
 import apiEndpoints from "../../../../endpoints.js/endpoints";
-import Venues from "../../Venues";
-import { Hero, HomeContainer, InputContainer, Search } from "./Home.styles";
 import Loader from "../../Loader";
 import Error from "../../Error";
+import Venues from "../../Venues";
+import { Hero, HomeContainer, Search } from "./Home.styles";
+import { InputContainer } from "../../../styles/Forms";
 
 export default function Home() {
-  const [showSearch, setShowSearch] = useState(false);
-  const [showPopup, setShowPopup] = useState(false);
   const [searchedVenue, setSearchedVenue] = useState("");
-  const [filteredVenue, setFilteredVenue] = useState("");
 
-  const {
-    fetchApi,
-    data: venues,
-    isLoading,
-    isError,
-    isSuccess,
-    errorMsg,
-  } = useApi();
+  const { fetchApi, data: venues, isLoading, isError } = useApi();
 
   const getData = useCallback(async () => {
     await fetchApi(apiEndpoints().venues);
@@ -43,121 +34,24 @@ export default function Home() {
   }
 
   return (
-    <>
-      {/* <FilterButton onClick={() => setShowPopup(!showPopup)}>
-        <FontAwesomeIcon icon={faFilter} />
-      </FilterButton>
-      <Popup className={showPopup ? "popup active" : "popup inactive"}>
-        <FontAwesomeIcon
-          className="close"
-          icon={faClose}
-          onClick={() => setShowPopup(false)}
-        />
-
-        <FilterContent>
-          <h2>Sort by</h2>
-          <select className="dropdown" name="" id="">
-            <option value="all">All</option>
-            <option value="lth">Price: low to high</option>
-            <option value="htl">Price: hight to low</option>
-            <option value="rating">Rating</option>
-          </select>
-
-          <section>
-            <h3>Price range</h3>
-          </section>
-
-          <section>
-            <h3>Max guests</h3>
-            <div>
-              <input type="radio" name="guests" />
-              <label htmlFor="one">1</label>
-            </div>
-            <div>
-              <input type="radio" name="guests" />
-              <label htmlFor="two">2</label>
-            </div>
-            <div>
-              <input type="radio" name="guests" />
-              <label htmlFor="three">3</label>
-            </div>
-            <div>
-              <input type="radio" name="guests" />
-              <label htmlFor="four">4</label>
-            </div>
-            <div>
-              <input type="radio" name="guests" />
-              <label htmlFor="five">5+</label>
-            </div>
-          </section>
-
-          <section>
-            <h3>Fascilities</h3>
-            <div className="fascContent">
-              <div className="fascBtn">
-                <input type="checkbox" name="wifi" />
-                <label htmlFor="wifi">Wifi</label>
-              </div>
-              <div className="fascBtn">
-                <input type="checkbox" name="breakfast" />
-                <label htmlFor="breakfast">Breakfast</label>
-              </div>
-              <div className="fascBtn">
-                <input type="checkbox" name="parking" />
-                <label htmlFor="parking">Parking</label>
-              </div>
-              <div className="fascBtn">
-                <input type="checkbox" name="pets" />
-                <label htmlFor="pets">Pets</label>
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <h3>Rating</h3>
-            <div>
-              <input type="radio" name="rating" />
-              <label htmlFor="one">1</label>
-            </div>
-            <div>
-              <input type="radio" name="rating" />
-              <label htmlFor="two">2</label>
-            </div>
-            <div>
-              <input type="radio" name="rating" />
-              <label htmlFor="three">3</label>
-            </div>
-            <div>
-              <input type="radio" name="rating" />
-              <label htmlFor="four">4</label>
-            </div>
-            <div>
-              <input type="radio" name="rating" />
-              <label htmlFor="five">5</label>
-            </div>
-          </section>
-        </FilterContent>
-      </Popup>  */}
-
-      <HomeContainer>
-        <Hero>
-          <Search>
-            <h1>Discover your next getaway</h1>
-            <InputContainer>
-              <FontAwesomeIcon icon={faSearch} />
-              <input
-                placeholder="Search venue"
-                type="search"
-                onChange={onSearch}
-                value={searchedVenue}
-                aria-label="Search"
-              ></input>
-            </InputContainer>
-          </Search>
-        </Hero>
-        {!searchedVenue && <Venues data={venues} />}
-        {searchedVenue && <Venues data={searchResults} />}
-      </HomeContainer>
-    </>
+    <HomeContainer>
+      <Hero>
+        <Search>
+          <h1>Discover your next getaway</h1>
+          <InputContainer>
+            <FontAwesomeIcon icon={faSearch} />
+            <input
+              placeholder="Search venue"
+              type="search"
+              onChange={onSearch}
+              value={searchedVenue}
+              aria-label="Search"
+            ></input>
+          </InputContainer>
+        </Search>
+      </Hero>
+      {!searchedVenue && <Venues data={venues} />}
+      {searchedVenue && <Venues data={searchResults} />}
+    </HomeContainer>
   );
 }
